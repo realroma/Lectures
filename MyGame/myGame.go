8048642eeb2a4ed47ed9437502c667b0b76bcfbd
realroma <real3060@yandex.ru>
2025-06-12 15:57:44 +0300

    Добавление адекватной обработки комманд из строки, в будущем добавится таймер для команды skip который будет учитывать разницу во временных отрезках. Это будет сделано через сравнение time.Duration() и time.ParseDuration()

--- MyGame/myGame.go
+++ MyGame/myGame.go
@@ -4,24 +4,35 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
-
-type Player struct {
-	Balance int
-	Buisnes *Busines
-}
-
-type Busines struct {
-	Name  string
-	Pay   int
-	Cost  int
-	Grade string
-}
 
 func NewP() Player {
 	b := DefaultB()
-	var p Player = Player{0, b}
+	var p Player = Player{0, b, time.Now()}
 	return p
+}
+
+type Player struct {
+	Balance  int
+	Buisnes  *Busines
+	Previous time.Time
+}
+
+func (p *Player) Work() {
+	p.Balance++
+}
+
+func (p *Player) Pay() {
+	now := time.Now()
+	if now.Sub(p.Previous) > 20 {
+		p.Balance = p.Balance + p.Buisnes.Pay
+		p.Previous = now
+	} else {
+		fmt.Println("Time less than 10.")
+	}
+	return
 }
 
 // Поинтеры нужны для того чтобы при изменении баланса у структуры, в мапе они так же изменялись, и не подвергались копированию.
@@ -33,18 +44,16 @@ func (p *Player) Buy(b *Busines) {
 	fmt.Println("Dont enough money")
 }
 
-func (p *Player) Work() {
-	p.Balance++
-}
-
-func (p *Player) Pay() *Player {
-	p.Balance = p.Balance + p.Buisnes.Pay
-	return p
-}
-
 func NewB(name string, cost int, pay int) *Busines {
 	b := Busines{Name: name, Cost: cost, Pay: pay, Grade: ""}
 	return &b
+}
+
+type Busines struct {
+	Name  string
+	Pay   int
+	Cost  int
+	Grade string
 }
 
 func DefaultB() *Busines {
@@ -56,6 +65,26 @@ func DefaultB() *Busines {
 
 var DefaultBusines Busines
 
+func handler(scan *bufio.Scanner, p *Player) {
+	for scan.Scan() {
+		text := strings.ToLower(scan.Text())
+		if text == "exit" {
+			fmt.Println("Exiting..")
+			return
+		} else if text == "work" {
+			fmt.Println("Player balance before: ", p.Balance)
+			p.Work()
+			fmt.Println("Player balance: ", p.Balance)
+		} else if text == "skip" {
+			fmt.Println("Player balance today: ", p.Balance)
+			p.Pay()
+			fmt.Println("Balance player: ", p.Balance)
+		} else {
+			fmt.Println("Wrong command.")
+		}
+	}
+}
+
 func main() {
 	//Создаём бизнес в доступе.
 	p := NewP()
@@ -65,18 +94,7 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println("Write command.")
 
-	for scanner.Scan() {
-		text := scanner.Text()
-		if text == "stop" {
-			break
-		} else if text == "work" {
-			fmt.Println("before balance ", p.Balance)
-			p.Work()
-			fmt.Println("after balance ", p.Balance)
-		} else if text == "skip" {
-			p.Pay()
-			fmt.Println("Day left. Your pay on come.")
-			fmt.Println(p.Balance)
-		}
-	}
+	handler(scanner, &p)
+
+	fmt.Println(p.Balance, "\n", &p.Buisnes)
 }
