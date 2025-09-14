ed693441f5dbf9076b12116ecf7cf7656862bac9
realroma <real3060@yandex.ru>
2025-09-14 03:15:16 +0300

    Text 2

--- MyGame/internal/player/service.go
+++ MyGame/internal/player/service.go
@@ -13,13 +13,7 @@ type service struct {
 	storage Storage
 }
 
-<<<<<<< HEAD
 func New(Id int) *Player {
 	var p Player = Player{Id, 0, *b.GetBusinesByName("None"), time.Now()}
 	return &p
-=======
-func New(Id int) Player {
-	var p Player = Player{Id, 0, *b.GetBusinesByName("None"), time.Now()}
-	return p
->>>>>>> 31e8e98fb86b970935d45d13dc767d9c2a0c7f2c
 }
--- work/Test2.go
+++ work/Test2.go
@@ -2,24 +2,29 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 var p float64 = 3.14159
 var cc float64 = 3.2
 
+func округление(val float64) float64 {
+	return math.Round(100*val) / 100
+}
+
 func кольцо(d float64) {
 	v := (d * p) / cc
-	fmt.Println("Количество циклов: ", v, " Уголо поворота стола: ", 360/(v*2))
+	fmt.Println("Количество циклов: ", округление(v), " Уголо поворота стола: ", округление(360/(v*2)))
 }
 
 var l, m, c, t float64
 
 func винт(h float64, d float64, v float64) {
 	l := p * d
-	m := 60 / (l / v)
+	m := (60 / (l / v)) * 100 / 32
 	c := l / v
 	t := (c / 1.6) * h
-	fmt.Println("l: ", l, " c: ", c, " o: ", (m*100)/32, " g: ", t/4, " f: ", t/3)
+	fmt.Println("l: ", округление(l), " c: ", округление(c), " o: ", округление((m*100)/32), " g: ", округление(t/4), " f: ", округление(t/3))
 }
 
 func main() {
